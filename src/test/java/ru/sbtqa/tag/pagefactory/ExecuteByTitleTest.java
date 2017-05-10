6e6c9c2d96db4fb612a3aa12ec12db179d048bc1
Varivoda Ivan <varivoda_ivan@mail.ru>
2017-05-10 17:58:13 +0300

    Create branch anr first test-template

---
+++ src/test/java/ru/sbtqa/tag/pagefactory/ExecuteByTitleTest.java
@@ -0,0 +1,28 @@
+package ru.sbtqa.tag.pagefactory;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Varivoda Ivan on 10.05.2017.
+ */
+public class ExecuteByTitleTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void executeMethodOnPageWithSpecifiedActionTitle() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
